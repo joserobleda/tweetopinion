f0440a09ab154212a3e9877f8b4ba81ef24ea8a6
Jose Ignacio Andres <joserobleda@gmail.com>
2014-03-19 21:15:05 +0100

    twitter data

--- models/user.js
+++ models/user.js
@@ -3,9 +3,24 @@
 	var Model 	= require('neasy/model');
 
 
+	var Twit = require('twit');
+
+	var T = new Twit({
+		consumer_key: 'nNhR0zSqlPU3r0m93AMcg', 
+		consumer_secret: 'IsVKN8TSyPwdNctNAF5q9QInARf2utkhbWZ7ss8E5o', 
+		access_token: '151658151-rOWMKHn2z5P1KU8d8J5U7ZN2n9E04X54Psvb8j6s', 
+		access_token_secret: 'dRiKAV9oAyzNpJkTTf3k35KuqlksY8CWyPT8ASyDrXm0x'
+	});
+
 
 	var User = Model.extend({
-		collection: 'users'
+		collection: 'users',
+
+		show: function (username) {
+			T.get('users/show', { screen_name: 'joserobleda' }, function (err, res) {
+				console.log(err, res);
+			});
+		}
 	});
 
 	User.class = 'users';
