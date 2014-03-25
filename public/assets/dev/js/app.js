a3fb0f9843e92fdae14d10c301a396f5c61174d2
Janayama <javier.anaya.martinez@gmail.com>
2014-03-25 18:01:35 +0100

    js limit control

--- public/assets/dev/js/app.js
+++ public/assets/dev/js/app.js
@@ -1,4 +1,30 @@
 (function (w, undefined) {
 	'use strict';
-	
+	var limit = 139;
+
+	$('.comment textarea').on('keypress paste', function (e) {
+		var length 		= $(this).val().length;
+		var remaining 	= limit-length;
+
+		if (length > limit) {
+			e.preventDefault();
+			$('.counter').first().addClass('red');
+		} else {
+			$('.counter').html(remaining);
+		}
+	});
+
+	$('.comment textarea').on('keyup', function (e) {
+		// capturing delete event. This does not work with keypress
+		var length 		= $(this).val().length;
+		var remaining 	= limit-length;
+		if(e.keyCode == 8) {
+			if (remaining > 0) {
+				$('.counter').first().removeClass('red');
+			}
+			$('.counter').html(remaining);
+		}
+	})
+
+
 }(window))
